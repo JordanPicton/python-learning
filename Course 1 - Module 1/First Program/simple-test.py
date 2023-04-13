d7b434256c667156e4517306bf3b8ee87c8d1a18
Jordan Picton <jordan_picton@hotmail.com>
2023-04-13 17:30:34 +0100

    Delete Course 1 - Module 1 directory
    No longer needed

--- Course 1 - Module 1/First Program/simple-test.py
+++
@@ -1,3 +0,0 @@
-print("Hello, this is my first String within Python3.")
-#This is how you do comments and below is something I want to keep across my works and hopefully find a better way in how to do it. :)
-print("Author: Jordan Picton  -  Date: 31/03/2023")
