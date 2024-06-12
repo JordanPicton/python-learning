c9528067e4db9e48aca9b1926418be2cc8e0b2b5
Jordan Picton <jordan_picton@hotmail.com>
2024-06-12 19:47:59 +0100

    Completed two more worksheet programs.

---
+++ Teesside-Uni/session_one/worksheet/program_2.py
@@ -0,0 +1,13 @@
+# Variable Creation
+
+greet = "Hello" # String variable.
+x = 23 # Integer variable.
+y = 3.0 # Float Variable.
+Z = 4 # Second Integer Variable.
+
+# Program
+name = input("Please enter your name: ") # Requires user input to store a value.
+print(greet, name) # Outputs the values from the variables greet and name.
+print("The output of Multipying", x, "by", y, "is:",x*y) # Outputs the String (green text) and variables X & Y whilst doing a calculation output of X & Y at the end.
+print("23 divide by 3.0 =", x/y) # This divides two numbers against each other.
+print("23 integer divide by 3.0", x//y) # This is an integer division, this rounds it seems.
---
+++ Teesside-Uni/session_one/worksheet/program_3.py
@@ -0,0 +1,16 @@
+# Variable Creation
+
+greet = "Hello" # String variable.
+x = 23 # Integer variable.
+y = 3.0 # Float Variable.
+z = 4 # Second Integer Variable.
+
+# Program
+#name = input("Please enter your name: ") # Requires user input to store a value.
+
+# Casting y variable as a integer.
+y = int(y)
+
+print(greet*3)
+print(greet*z)
+print(greet*y) # This causes an error because it's a float and should be an integer.
