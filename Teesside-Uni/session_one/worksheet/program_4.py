50e786d132625eb15e48f6a595971c25eaecb952
Jordan Picton <jordan_picton@hotmail.com>
2024-06-12 19:52:40 +0100

    Completed Worksheet program4.

---
+++ Teesside-Uni/session_one/worksheet/program_4.py
@@ -0,0 +1,5 @@
+# Main Program
+entry = int(input("Please input a number: ")) # Requires user input to store value into entry variable.
+
+square = entry**2 # This takes the value from entry and squares it, giving a new value to the variable sqaure.
+print(square) # Prints out square value.
