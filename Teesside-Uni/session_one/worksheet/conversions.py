5a695ae61025aad406240cb3784647f3e474e3fe
Jordan Picton <jordan_picton@hotmail.com>
2024-06-12 20:41:13 +0100

    Completed Worksheet1 conversion program.

---
+++ Teesside-Uni/session_one/worksheet/conversions.py
@@ -0,0 +1,7 @@
+# Defining Variables
+kmph = float(input("Please insert your km/h: "))
+
+# Main Program
+mps = kmph * 0.27778 # Kmph calculation to mps.
+mps = round(mps, 5)
+print(kmph, "km/h is equal to", mps,"m/s.") # Output of the calculatiuon value.
