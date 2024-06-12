da39342b4316d5347131a4c82e1b85a2f375d5b8
Jordan Picton <jordan_picton@hotmail.com>
2024-06-12 19:26:10 +0100

    Completed First user input example program.

---
+++ Teesside-Uni/session_one/username-input.py
@@ -0,0 +1,6 @@
+# Takes input from the console from the user.
+name = input("Please enter your name: ")
+# Uses that input stored as name and prints it out into console, using the variable name.
+print ("Hello", name)
+# The example below shows how adding quotes can mess this up.
+print ("Hello, this is the example that shows adding quotes breaks it.", "name")
