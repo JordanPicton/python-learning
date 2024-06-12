3991054de4fb0cf7191b8df77d86d10768f26802
Jordan Picton <jordan_picton@hotmail.com>
2024-06-12 20:11:50 +0100

    Completed Worksheet1 sum and product program.

---
+++ Teesside-Uni/session_one/worksheet/sum_and_product.py
@@ -0,0 +1,10 @@
+# Defining Variables:
+intOne = int(input("Please input your first number: "))
+intTwo = int(input("Please input your second number: "))
+intThree = int(input("Please input your third number: "))
+
+# Main Program
+sumAnswer = intOne + intTwo + intThree # Adds the three values together.
+productAnswer = intOne * intTwo * intThree # Multpilies the three values together.
+print("The sum is", sumAnswer, "and the product is", productAnswer)
+print("Program completed!")
