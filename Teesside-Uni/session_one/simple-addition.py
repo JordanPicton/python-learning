83a1bddd947a82191dc919ce89388ed3e7320583
Jordan Picton <jordan_picton@hotmail.com>
2024-06-12 19:25:47 +0100

    Completed 2 more example programs.

---
+++ Teesside-Uni/session_one/simple-addition.py
@@ -0,0 +1,6 @@
+x = 82
+y = 22
+z = x+y
+# Print out the calculation of X + Y
+print("The answer between " + str(x) + " and " + str(y) + " comes to: " + str(z))
+print("The answer between",x,"and",y,"comes to:",z)
