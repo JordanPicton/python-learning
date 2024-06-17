3d183096e63d84d207f7e399c46e3cd2a81b8b59
Jordan Picton <jordan_picton@hotmail.com>
2024-06-17 19:45:01 +0100

    Completed cylindervolume program.

---
+++ Teesside-Uni/session_one/worksheet_personal_answers/cyclindervolume.py
@@ -0,0 +1,15 @@
+# Importing Libs
+import math
+
+# Defining Variables
+pi = math.pi
+
+# User Input
+radius = float(input("Please enter the radius: "))
+height = float(input("Please enter the height: "))
+
+# Main Program
+volume = pi * radius ** 2 * height
+
+# Output
+print("The volume of the cylinder is:", volume)
