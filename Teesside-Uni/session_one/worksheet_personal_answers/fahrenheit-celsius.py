2bbbd46b4bb22c6357acfe9feac589c879467b58
Jordan Picton <jordan_picton@hotmail.com>
2024-06-17 20:25:50 +0100

    Completed fahrenheit-celsius program.

---
+++ Teesside-Uni/session_one/worksheet_personal_answers/fahrenheit-celsius.py
@@ -0,0 +1,8 @@
+# Defining Variables
+fahrenheit = float(input("Please enter the Fahrenheit temperature you wish to convert: "))
+
+# Main Program
+celsius = (fahrenheit - 32) * 5/9
+
+# Output
+print(fahrenheit, "degrees Fahrenheit is the equivelant to", celsius, "degrees Celsius")
