056aa810c7d4135720fd452ed42d3e9c9f1afce4
Jordan Picton <jordan_picton@hotmail.com>
2024-06-17 21:01:14 +0100

    Completed celsius-fahrenheit program.

--- Teesside-Uni/session_one/worksheet_personal_answers/celsius-fahrenheit.py
+++ Teesside-Uni/session_one/worksheet_personal_answers/celsius-fahrenheit.py
@@ -1,4 +1,8 @@
-# Importing Libs
-import math
+# User Input
+celsius = float(input("Please enter the Celsius temperature you would like to convert into Fahrenheit: "))
 
-# Defining Variables
+# Main Program
+fahrenheit = (celsius * 9/5) + 32
+
+# Output
+print(celsius, "degrees Celsius is the equivelant to", fahrenheit, "degrees Fahrenheit.")
