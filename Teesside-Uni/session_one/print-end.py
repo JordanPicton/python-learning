d4d1921086a83e87f7c8c50b8281c26d1c9c45f0
Jordan Picton <jordan_picton@hotmail.com>
2024-06-22 05:05:42 +0100

    Added some programs to explain some print conditions.

---
+++ Teesside-Uni/session_one/print-end.py
@@ -0,0 +1,11 @@
+# Example to show off the end condition for the print key word.
+# Normal Print
+print("(First Statement) This is how print")
+print("(Second Statement) looks normally.")
+
+# Breaker
+print("------------")
+
+# Print with changing the end condition.
+print("(1st Statement) This is how print looks with the", end="")
+print("(2nd Statement) end condition set to an empty string.")
---
+++ Teesside-Uni/session_one/print-sep.py
@@ -0,0 +1,6 @@
+# Example to show off the sep (separation) condition for the print key word.
+# Normal Print
+print("This is how", "Strings look normally", "when being concatenated.")
+
+# Changing the separation condiotion. Uses "-" characters isntead of the default spaces.
+print("This is how", "Strings look when", "changing the sep condition.", "Using the '-' character.", sep="-")
