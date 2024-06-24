ff996032f1e5b66c1c113f1cebc51f2361fee2e9
Jordan Picton <jordan_picton@hotmail.com>
2024-06-24 18:37:11 +0100

    Completed Boolean program.

---
+++ Teesside-Uni/session_two/boolean-logic.py
@@ -0,0 +1,16 @@
+# Boolean Logic
+print("The Hello is valued as a", type("Hello"), "data type.")
+print("5 is valued as a", type(5), "data type.")
+print("False is valued as a", type(False), "data type.")
+print("True AND True result in a", True and True, "output.")
+print("True AND False result in a", True and False, "output.")
+print("True OR False result in a", True or False, "output.")
+print("True OR True result in a", True or True, "output.")
+print("False OR False result in a", False or False, "output.")
+print("NOT True is", not True)
+print("NOT False is", not False)
+print("NOT NOT NOT NOT True is", not not not not True)
+
+# AND = Conjunction
+# FALSE = Disjunction.
+# NOT = Negation.
