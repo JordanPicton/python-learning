cb3e31b648d192b9d170cb2db2f40d5d5abdbd02
Jordan Picton <jordan_picton@hotmail.com>
2024-07-03 17:11:16 +0100

    Needs more work

--- Teesside-Uni/session_two/while-if-loop.py
+++ Teesside-Uni/session_two/while-if-loop.py
@@ -1,6 +1,10 @@
 # Validation Loop
 # 03/07/2024 - Jordan Picton
-userInput = input("Please enter your age?")
 
-while userInput == None:
-    print("I need you to give me an input")
+# Delcaring user input into a variable.
+userInput = input("Please enter your age: ")
+
+# Checks to see if the input is empty, making it result true is so.
+while userInput == None or not userInput:
+    # Run the code below if the statement above is true.
+    input("Please enter your age: ")
