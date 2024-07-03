946c0e49ea4d477073971bf789009c964ebee91b
Jordan Picton <jordan_picton@hotmail.com>
2024-07-03 19:19:08 +0100

    Added nested IF Statement example.

--- Teesside-Uni/session_two/if-statement.py
+++ Teesside-Uni/session_two/if-statement.py
@@ -18,3 +18,14 @@ if numberOne > 100 and numberOne % 2 == 0:
   print(numberOne, "is a very cool number!") # Prints out that this is a cool number.
 else:
  print(numberOne, "is a very uncool number.") # Prints out that this is an uncool number.
+
+# Age verifification IF Statement.
+age = int(input("Enter your age: ")) # Declaring variable with user input which is casted to an integer.
+if age < 18: # Checks if the value of age is less than 18.
+  adult = input("Do you have an adult with you?").lower() # Declares a variable with user input that uses the lower() method.
+  if adult != "yes": # Checks if the answer give from the variable created above has the anything other than "yes".
+    # If anything other than yes is given then continue running the code within this block.
+    print("Access Denied!") # Print out Access Denied message.
+    exit(0) # Exit the program. (Skips the rest of the code below for this if Statement.) This part is interesting.
+  else: # If the answer is yes then continue with the code below.
+    print("You can proceed.") # Print out You can proceed.
