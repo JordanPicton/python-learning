85fba9227de691800bcd721ad734e40f2ff7387f
Jordan Picton <jordan_picton@hotmail.com>
2024-07-10 11:45:42 +0100

    More work on sorting list.

--- Teesside-Uni/session_two/worksheet_personal_answers/conditional_sorting.py
+++ Teesside-Uni/session_two/worksheet_personal_answers/conditional_sorting.py
@@ -1,27 +1,30 @@
 # Conditional Sorting Program
 # Jordan Picton 03/07/2024
 
-# Declare variables. (User inputs)
-# numOne = int(input("Please insert your first number: ")) # Takes user input to give the variable a value.
-# numTwo = int(input("Please insert your second number: ")) # Takes user input to give the variable a value.
-# numThree = int(input("Please insert your third number: ")) # Takes user input to give the variable a value.
-
 flag = True
 
 # Declare variables
 numList = []  # Creating a new empty list.
 
-numOneList = input(
-    "Please insert your third number or 'exit' to quit the program: ")
+numOne = input(
+    "Please insert your delivery weight here or 'exit' to quit the program: ")
+numList.append(int(numOne))# Adds the value from numOne into the numList list.
+
+print(numList)
 
 while flag:
       # Takes user input to give the variable a value.
-      numOne = input("Please insert your first number or 'exit' to quit the program: ")
+      numOne = input("Please insert your next delivery weight here or 'exit' to quit the program: ")
+      numList.append(int(numOne))# Adds the value from numOne into the numList list.
       # Checks if the value of
       if numOne == "exit":
         flag = False
       else:
-        numList.append(int(numOne))# Adds the value from numOneList into the numList list.
-        numTwo = input("Please insert your second number or 'exit' to quit the program: ")
-        if numTwo == "exit":
+        numList.append(int(numOne))# Adds the value from numOne into the numList list.
+        numOne = input("Please insert your next delivery weight here or 'exit' to quit the program: ")
+        if numOne == "exit":
+          flag = False
           print(numList)
+          
+numList.sort()
+print(numList)
