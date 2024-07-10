0c1c56e2533232114d01e2bef4e67b51dfb9ff77
Jordan Picton <jordan_picton@hotmail.com>
2024-07-10 15:22:40 +0100

    Start on sum_even program.

--- Teesside-Uni/session_two/worksheet_personal_answers/sum_even.py
+++ Teesside-Uni/session_two/worksheet_personal_answers/sum_even.py
@@ -1,2 +1,15 @@
 # Sum Even Program
-# Jordan Picton 04/07/2024
+# Jordan Picton 10/07/2024
+
+# Declaring the list of values.
+integers = [14,5,19,20,21,66,89]
+
+# Declaring a total value at 0.
+total = 0
+
+# Initialising a counter with the value of 0.
+counter = 0
+
+while(counter < len(integers)):
+    if counter % 2 == 0:
+        total = total + integers[counter]
