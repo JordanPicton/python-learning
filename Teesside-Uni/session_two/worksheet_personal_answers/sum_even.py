84b2be2771155dcb3dd213010a37c29c4c738170
Jordan Picton <jordan_picton@hotmail.com>
2024-07-11 17:20:32 +0100

    Completed sum_even program.

--- Teesside-Uni/session_two/worksheet_personal_answers/sum_even.py
+++ Teesside-Uni/session_two/worksheet_personal_answers/sum_even.py
@@ -10,6 +10,13 @@ total = 0
 # Initialising a counter with the value of 0.
 counter = 0
 
+# While counter is less than the length of the integers list run the code below:
 while(counter < len(integers)):
+    # If counter is divisible by 2 with no remainder then run the code below:
     if counter % 2 == 0:
+        # total is given a new value which is the previous total value plus the value that counter is at within the integers list.
         total = total + integers[counter]
+    # Increase counter by 1.
+    counter += 1
+# Print out the total.
+print("The total is:", str(total))
